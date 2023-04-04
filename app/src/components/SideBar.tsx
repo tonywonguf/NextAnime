3f0050ff224687b2211654831ec16609db0a62e7
Tony Wong <t.wong0902@gmail.com>
2023-04-04 06:03:57 -0400

    final aesthetic fix before i knockout :sleep:

--- app/src/components/SideBar.tsx
+++ app/src/components/SideBar.tsx
@@ -1,6 +1,6 @@
 export default function SideBar(props) {
     return (
-        <div className={"flex-shrink-0 bg-purple-200 w-96 m-1 rounded"}>
+        <div className={"flex-shrink-0 bg-purple-200 flex-grow m-1 rounded"}>
             <p>test</p>
         </div>
     );
