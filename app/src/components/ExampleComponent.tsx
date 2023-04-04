2c1c8996f1fcb49a203f9ab590c34c0b845d3a9c
Robert Conde <rconde2003@gmail.com>
2023-04-03 20:56:03 -0400

    ExampleComponent & Setup TailwindCSS

---
+++ app/src/components/ExampleComponent.tsx
@@ -0,0 +1,36 @@
+import {Component} from "react";
+
+interface Props {
+    startNum: number
+}
+
+interface States {
+    num: number
+}
+
+export default class ExampleComponent extends Component<Props, States> {
+    constructor(props) {
+        super(props);
+        this.state = {num: this.props.startNum};
+    }
+
+    increment() {
+        this.setState({num: this.state.num + 1});
+    }
+
+    render() {
+        return (
+            <div>
+                <p className={"mb-2"}>
+                    <b>Current #:</b> <a className={"font-mono"}>{this.state.num}</a>
+                </p>
+
+                <button onClick={this.increment.bind(this)}
+                        className={"bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"}
+                >
+                    Increment
+                </button>
+            </div>
+        )
+    }
+}
--- app/src/index.js
+++ app/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ExampleComponent from "./components/ExampleComponent";
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <div className={"p-1"}>
+            <ExampleComponent startNum={0}/>
+        </div>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
