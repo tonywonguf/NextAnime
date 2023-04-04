a5abe0d6a6d208da773b83a3d5cc3135a90298bb
Tony Wong <t.wong0902@gmail.com>
2023-04-03 21:23:48 -0400

    scrape

---
+++ scrape/scrape/scrape.py
@@ -0,0 +1,79 @@
+import requests
+import time
+import json
+
+# Here we define our query as a multi-line string
+query = '''
+query ($page: Int) {
+    Page(page: $page){ # Insert our variables into the query arguments (page) (type: ANIME is hard-coded in the query)
+        pageInfo{
+            lastPage
+        }
+        media{
+            id
+            title{
+                english
+                romaji
+                native
+            }
+            genres
+            popularity
+            type
+            episodes
+            seasonYear
+            chapters
+            coverImage{
+                medium
+            }
+            trending
+            staff{
+                nodes{
+                    name{
+                        full
+                    }
+                }
+            }
+            studios{
+                nodes{
+                    name
+                }
+            }
+        }
+    }
+}
+'''
+variables = {
+    'page': 1
+}
+url = 'https://graphql.anilist.co'
+
+data = []
+# Make the HTTP Api request
+response = requests.post(url, json={'query': query, 'variables': variables})
+parseResponse = (response.json())
+#print(parseResponse)
+for x in range(parseResponse['data']['Page']['pageInfo']['lastPage']):
+    variables = {
+        'page': x
+    }
+    response = requests.post(url, json={'query': query, 'variables': variables})
+    parseResponse = (response.json())
+    #print(parseResponse)
+    #print(parseResponse['data'])
+    if(not(parseResponse['data'] == None)):
+        desiredMedia = parseResponse['data']['Page']['media']
+        #print((desiredMedia))
+        #print(desiredMedia[1])
+        #print(desiredMedia[1]['id'])
+        for k in range(len(desiredMedia)) :
+            if(not(desiredMedia == None)):
+                lis = []
+                for keys in desiredMedia[k].keys():
+                    lis.append(desiredMedia[k][keys])
+                    #data.append((desiredMedia[k]['id'], desiredMedia[k]['title'], desiredMedia[k]['genres'], desiredMedia[k]['popularity']))
+                data.append(lis)
+    #print(response.json())
+    time.sleep(60/90)
+
+for d in range(len(data)//8):
+    print(data[d])
