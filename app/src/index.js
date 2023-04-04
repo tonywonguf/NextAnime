63263b1581cb8618fece7f510618c4dba5fb1dde
Robert Conde <rconde2003@gmail.com>
2023-04-04 02:29:48 -0400

    Remove Strict Mode

--- app/src/index.js
+++ app/src/index.js
@@ -6,11 +6,11 @@ import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
+    // <React.StrictMode>
         <div className={"p-1"}>
             <ExampleComponent startNum={1}/>
         </div>
-    </React.StrictMode>
+    // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
