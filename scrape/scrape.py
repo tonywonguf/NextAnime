fbe553199c424552b022bc4d52fb31ee8611dde0
Tony Wong <t.wong0902@gmail.com>
2023-04-23 14:00:40 -0400

    fixed so scrapper gets large image and also iterates through every page using hasNextPage!
    now there are 2215 pages to go and get! thats 50*2215 anime/manga/hentai

--- scrape/scrape.py
+++ scrape/scrape.py
@@ -3,7 +3,7 @@ import time
 import json
 
 # Here we define our query as a multi-line string
-query = '''
+query ='''
 query ($page: Int) {
     Page(page: $page){ # Insert our variables into the query arguments (page) (type: ANIME is hard-coded in the query)
         pageInfo{
@@ -24,6 +24,7 @@ query ($page: Int) {
             chapters
             coverImage{
                 medium
+                large
             }
             trending
             staff{
@@ -53,34 +54,31 @@ data = []
 # Make the HTTP Api request
 response = requests.post(url, json={'query': query, 'variables': variables})
 parseResponse = (response.json())
-#print(parseResponse)
 
 x = 1
 while (parseResponse['data']['Page']['pageInfo']['hasNextPage']):
+    if x == 150:
+        break
     variables = {
         'page': x
     }
     response = requests.post(url, json={'query': query, 'variables': variables})
     parseResponse = (response.json())
-    #print(parseResponse)
-    #print(parseResponse['data'])
+
     if(not(parseResponse['data'] == None)):
         desiredMedia = parseResponse['data']['Page']['media']
-        #print((desiredMedia))
-        #print(desiredMedia[1])
-        #print(desiredMedia[1]['id'])
+
         for k in range(len(desiredMedia)) :
             if(not(desiredMedia == None)):
                 lis = []
                 for keys in desiredMedia[k].keys():
                     lis.append(desiredMedia[k][keys])
-                    #data.append((desiredMedia[k]['id'], desiredMedia[k]['title'], desiredMedia[k]['genres'], desiredMedia[k]['popularity']))
                 data.append(lis)
-    #print(response.json())
     print("Completed", len(data))
     time.sleep(60/90)
     x += 1
 
-
+print("Successful!\nType any key to exit: ")
+exit = input()
 with open("data.json", "w+") as f:
    json.dump(data, f)
