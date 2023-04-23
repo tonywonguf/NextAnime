4042a7301d1ff4b8f570e13d786d64d38837d758
Tony Wong <t.wong0902@gmail.com>
2023-04-23 01:44:29 -0400

    life and death and angles

--- app/src/components/Datafile.tsx
+++ app/src/components/Datafile.tsx
@@ -1,7 +1,5 @@
 import animedata from "../data/animedata.json";
-import SideBar from "./SideBar";
 
-let nodes = [];
 export type NodeInfo = {
     id?
     label?
@@ -46,27 +44,23 @@ export class Node {
     }
 }
 
-export function initNodes() {
-    //let nodes : Node[] = [];
-    for(let i = 0; i < animedata.length; i++){
-        let node : Node = new Node({
-            id: animedata[i][0],
-            label: animedata[i][1]["english"]??animedata[i][1]["native"],
-            tags: animedata[i][2],
-            popScore: animedata[i][3],
-            mediaType: animedata[i][4],
-            episodes: animedata[i][5],
-            year: animedata[i][6],
-            chapters: animedata[i][7],
-            image: animedata[i][8]["medium"],
-            trendScore: animedata[i][9],
-            staff: animedata[i][10]["nodes"],
-            studios: animedata[i][11]["nodes"]
-        });
-        nodes.push(node);
-    }
+export const nodes = [];
+for(let i = 0; i < animedata.length; i++){
+    nodes.push(
+    new Node({
+        id: animedata[i][0],
+        label: animedata[i][1]["english"],
+        tags: animedata[i][2],
+        popScore: animedata[i][3],
+        mediaType: animedata[i][4],
+        episodes: animedata[i][5],
+        year: animedata[i][6],
+        chapters: animedata[i][7],
+        image: animedata[i][8]["medium"],
+        trendScore: animedata[i][9],
+        staff: animedata[i][10]["nodes"],
+        studios: animedata[i][11]["nodes"]
+    }));
 }
+export const edges = [];
 
-initNodes();
-
-export {nodes};
