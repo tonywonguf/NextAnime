9bc48cadd0d1292797d35fbac81c161db0958cea
Tony Wong <t.wong0902@gmail.com>
2023-04-24 03:48:07 -0400

    added Edge class
    and made nodes and edges DataSet

--- app/src/components/Datafile.tsx
+++ app/src/components/Datafile.tsx
@@ -1,4 +1,5 @@
 import animeData from "../data/animedata.json";
+import {DataSet} from 'vis-data'
 
 export class Node {
     id?
@@ -32,13 +33,30 @@ export class Node {
     }
 }
 
-export const nodes = [];
+export class Edge {
+    from?
+    to?
+    id?
+    color?
+    weight?
+
+    constructor(info : Edge) {
+        this.from = info.from
+        this.to = info.to
+        this.id = info.id
+        this.color = info.color
+        this.weight = info.weight
+    }
+}
+
+
+export const nodes : DataSet<Node> = new DataSet<Node>();
 // @ts-ignore
 for(let i = 0; i < animeData.length; i++){
 
-    nodes.push(
+    nodes.add(
     new Node({
-        id: animeData[i][0],
+        id: i,
         label: (animeData[i][1]["english"]??animeData[i][1]["romaji"]).trim(),
         titles: animeData[i][1],
         tags: animeData[i][2],
@@ -46,21 +64,18 @@ for(let i = 0; i < animeData.length; i++){
         episodes: animeData[i][4],
         seasonYear: animeData[i][5],
         chapters: animeData[i][6],
-        image: animeData[i][7]["medium"],
+        image: animeData[i][7]["large"],
         imageMedium: animeData[i][7]["medium"],
         imageLarge: animeData[i][7]["large"],
         studios: animeData[i][8]["nodes"],
         isAdult: animeData[i][9]
     }));
     // turns the picture into a cat if it's BAD BAD!
-    if(nodes.at(-1).isAdult){
-        nodes.at(-1)["image"] = 'https://www.shutterstock.com/image-photo/little-beautiful-funny-british-kitten-260nw-1521783215.jpg';
-        nodes.at(-1)["imageLarge"] = 'https://www.shutterstock.com/image-photo/little-beautiful-funny-british-kitten-260nw-1521783215.jpg';
-        console.log(nodes.at(-1)["label"]) // print out the BAD BAD images
+    if(nodes.get(i).isAdult){
+        nodes.get(i)["image"] = 'https://www.shutterstock.com/image-photo/little-beautiful-funny-british-kitten-260nw-1521783215.jpg';
+        nodes.get(i)["imageLarge"] = 'https://www.shutterstock.com/image-photo/little-beautiful-funny-british-kitten-260nw-1521783215.jpg';
     }
 }
 
-nodes.sort((l, r) => l.label.localeCompare(r.label))
-
-export const edges = [];
+export const edges : DataSet<Edge> = new DataSet<Edge>();
 
